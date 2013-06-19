2b12c530009af139757c529deae9475d21c5e55b
Łukasz Marjański <lukasz.marjanski@etendard.pl>
2013-06-19 17:01:51 +0200

    Initial Commit

---
+++ halohalo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  halohalo
+//
+//  Created by Łukasz Marjański on 19.06.2013.
+//  Copyright (c) 2013 Łukasz Marjański. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
